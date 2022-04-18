0ade7f6ea3e4876a30bdad7f724def375ced44e9
liuqiqi-Y <xiaoliuliu777@outlook.com>
2022-04-18 23:57:58 +0800

    continue

--- web-server/main.go
+++ web-server/main.go
@@ -11,16 +11,30 @@ func Hello(w http.ResponseWriter, r *http.Request) {
 }
 
 func login(w http.ResponseWriter, r *http.Request) {
-	err := r.ParseForm()
-	if err != nil {
-		fmt.Fprintf(w, fmt.Sprintln("have no name"))
-	}
-	fmt.Fprintf(w, fmt.Sprintf("well come %s", r.Form["name"][0]))
+	// err := r.ParseForm()
+	// if err != nil {
+	// 	fmt.Fprintf(w, fmt.Sprintln("have no name"))
+	// }
+	// contents, err := ioutil.ReadFile(`.\w\x8yexx`)
+	// if err != nil {
+	// 	fmt.Fprintf(w, err.Error())
+	// 	return
+	// }
+	fmt.Fprintf(w, `<!DOCTYPE html>
+	<html>
+	<head>
+	<meta charset="utf-8">
+	<title>菜鸟教程(runoob.com)</title>
+	</head>
+	<body>
+		<h1>我的第一个标题</h1>
+		<p>我的第一个段落。</p>
+	</body>
+	</html>`)
 }
 
 func main() {
-	http.HandleFunc("/", Hello)
-	http.HandleFunc("/user/login", login)
+	http.HandleFunc("/w", login)
 	err := http.ListenAndServe("0.0.0.0:8880", nil)
 	if err != nil {
 		fmt.Println("http listen failed")
